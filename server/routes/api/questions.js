ecdaa501d7f92977c3dbb70dc91d2ba1c3c45276
Tavonga Tawonezvi <ttawonezvi13@gmail.com>
2024-07-14 20:38:32 +0200

    added multiple api endpoints

--- server/routes/api/questions.js
+++ server/routes/api/questions.js
@@ -3,18 +3,32 @@ const mongodb = require ('mongodb');
 
 const router = express.Router();
 
-// Get Questions
-router.get('/', async (req, res) => {
-    const questions = await loadQuestionsCollections();
-    res.send(await questions.find({}).toArray());
-})
 
-
-module.exports = router;
-
+// Connect to the database
 async function loadQuestionsCollections() {
     const client = await mongodb.MongoClient.connect
     ('mongodb+srv://ttawonezvi13:eGwhbFf9PpFIRvW6@cluster0.gq7ucfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
     return client.db('OfficeTrivia').collection('questions');
 }
+
+// Get Questions
+router.get('/random', async (req, res) => {
+    const questions = await loadQuestionsCollections();
+    res.send(await questions.find({}).toArray());
+})
+router.get('/easy', async (req, res) => {
+    const questions = await loadQuestionsCollections();
+    res.send(await questions.find({'difficulty': 'easy'}).toArray());
+})
+router.get('/medium', async (req, res) => {
+    const questions = await loadQuestionsCollections();
+    res.send(await questions.find({'difficulty': 'medium'}).toArray());
+})
+router.get('/hard', async (req, res) => {
+    const questions = await loadQuestionsCollections();
+    res.send(await questions.find({'difficulty': 'hard'}).toArray());
+})
+
+
+module.exports = router;
