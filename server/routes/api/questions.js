8ca7bb006d087408084efb87d9f719b23a885de0
Tavonga Tawonezvi <ttawonezvi13@gmail.com>
2024-07-29 17:05:10 +0200

    added endpoint to post potential questions

--- server/routes/api/questions.js
+++ server/routes/api/questions.js
@@ -4,13 +4,21 @@ require('dotenv').config();
 const router = express.Router();
 
 
-// Connect to the database
+// Connect to the database (questions collection)
 async function loadQuestionsCollections() {
     const client = await mongodb.MongoClient.connect
     (`mongodb+srv://${process.env.MONGO_CON}.gq7ucfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
 
     return client.db('OfficeTrivia').collection('questions');
 }
+// Connect to the database (recommendedQuestions collection)
+async function loadRecommendedQuestionsCollections() {
+    const client = await mongodb.MongoClient.connect
+    (`mongodb+srv://${process.env.MONGO_CON}.gq7ucfk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+
+    return client.db('OfficeTrivia').collection('recommendedQuestions');
+}
+
 
 // Get Questions
 router.get('/random', async (req, res) => {
@@ -30,5 +38,13 @@ router.get('/hard', async (req, res) => {
     res.send(await questions.find({'difficulty': 'hard'}).toArray());
 })
 
-
+// Add recommeneded questions to the recommendedQuestions collection
+router.post('/users/recommended', async (req, res) => {
+    const recommendedQuestions = await loadRecommendedQuestionsCollections();
+    await recommendedQuestions.insertOne({
+        question: req.body.question,
+        answer: req.body.answer,
+    }); 
+    res.status(201).send();
+});
 module.exports = router;
